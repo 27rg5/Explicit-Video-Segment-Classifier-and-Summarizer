a9aa6e8778555d19c427eae463707148750055a2
Shaunak Sachin Joshi <shaunakjoshi12@gmail.com>
2023-11-11 19:25:28 -0800

    Add support for testing different modality combinations

--- eval.py
+++ eval.py
@@ -14,11 +14,16 @@ from torch.utils.data import DataLoader
 from models import LanguageModel, UnifiedModel, SpectrogramModel
 from torcheval.metrics.functional import multiclass_f1_score
 
-def inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, checkpoint_path, root_dir_path, EncodeVideo_obj, vanilla_fusion=False, pairwise_attention_modalities=False, experiment_name=None, get_classified_list=None, language_model_name='distilbert-base-uncased', video_model_name='slowfast_r50', spectrogram_model_name='resnet18', device='cuda:0'):
+def inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, checkpoint_path, root_dir_path, EncodeVideo_obj, modalities=['video','audio','text'], vanilla_fusion=False, pairwise_attention_modalities=False, experiment_name=None, get_classified_list=None, language_model_name='distilbert-base-uncased', video_model_name='slowfast_r50', spectrogram_model_name='resnet18', device='cuda:0'):
 
-    LanguageModel_obj = LanguageModel(model_name = language_model_name)
-    VideoModel_obj = VideoModel(model_name = video_model_name)
-    SpectrogramModel_obj = SpectrogramModel(model_name = spectrogram_model_name)
+    LanguageModel_obj, VideoModel_obj, SpectrogramModel_obj = None, None, None
+    if 'text' in modalities:
+        LanguageModel_obj = LanguageModel(model_name = language_model_name)
+    if 'video' in modalities:
+        VideoModel_obj = VideoModel(model_name = video_model_name)
+    if 'audio' in modalities:
+        SpectrogramModel_obj = SpectrogramModel(model_name = spectrogram_model_name)
+
     batch_size = 1
     softmax = nn.Softmax(dim=1)
     if pairwise_attention_modalities:
@@ -28,11 +33,16 @@ def inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, c
     elif vanilla_fusion:
         #Baseline
         in_dims = None
-        out_dims = 600
+        if len(modalities)==2:
+            out_dims = 400
+        elif len(modalities)==1:
+            out_dims = 200
+        else:
+            out_dims = 600
     else:
-        #Concatenate and then self-attention
+        #Concate and then self-attention
         in_dims = 600
-        out_dims = 600      
+        out_dims = 600    
 
     intermediate_dims = 50
     self_attention = not pairwise_attention_modalities
@@ -47,8 +57,10 @@ def inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, c
     'root_dir':root_dir_path,
     'all_encoded_videos':val_videos,
     'encoded_video_obj':EncodeVideo_obj,
-    'device':device
+    'device':device,
+    'modalities':modalities
     }
+
     val_dataset = VideoClipDataset(**val_dataset_dict)
     val_dataloader = DataLoader(val_dataset, shuffle=False, batch_size=batch_size, pin_memory=True)
     classes = val_dataset.classes
@@ -59,12 +71,16 @@ def inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, c
     targets_val = list()
     
     videos = list()
+
     for i, modality_inputs in tqdm(enumerate(val_dataloader)):
         with torch.no_grad():
             video_path, transformed_video, processed_speech, spectrogram_enc, target = modality_inputs
 
-            transformed_video = [elem.to(device, non_blocking=True) for elem in transformed_video]
-            processed_speech = {key:processed_speech[key].to(device, non_blocking=True) for key in processed_speech.keys()}
+            if transformed_video!=0:
+                transformed_video = [elem.to(device, non_blocking=True) for elem in transformed_video]
+            if processed_speech!=0:
+                processed_speech = {key:processed_speech[key].to(device, non_blocking=True) for key in processed_speech.keys()}
+
             spectrogram_enc = spectrogram_enc.to(device, non_blocking=True)
             target = target.to(device, non_blocking=True)
 
@@ -74,6 +90,7 @@ def inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, c
             preds_val.append(pred_softmax.cpu().item())
             targets_val.append(target.cpu().item())
             videos.append(video_path)
+        
     targets_val = torch.tensor(targets_val)
     preds_val = torch.tensor(preds_val)
 
@@ -87,8 +104,8 @@ def inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, c
         predictions_df.to_csv(os.path.join(os.getcwd(),'runs',experiment_name,'predictions.csv'), index=False)
 
 
-    print('f1-score macro:{} f1-score micro:{} f1-score weighted:{} accuracy:{}'.format(round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="macro").item(), 2), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="micro").item(), 2), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="weighted").item(), 2), round((preds_val==targets_val).sum()/len(targets_val), 2)))
-    print('f1-score macro:{} f1-score micro:{} f1-score weighted:{} accuracy:{}'.format(round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="macro").item(), 3), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="micro").item(), 3), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="weighted").item(), 3), round((preds_val==targets_val).sum()/len(targets_val), 3)))
+    print('f1-score macro:{} f1-score micro:{} f1-score weighted:{} accuracy:{}'.format(round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="macro").item(), 2), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="micro").item(), 2), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="weighted").item(), 2), round(((preds_val==targets_val).sum()/len(targets_val)).item(), 2)))
+    print('f1-score macro:{} f1-score micro:{} f1-score weighted:{} accuracy:{}'.format(round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="macro").item(), 3), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="micro").item(), 3), round(multiclass_f1_score(preds_val, targets_val, num_classes=2, average="weighted").item(), 3), round(((preds_val==targets_val).sum()/len(targets_val)).item(), 3)))
     
 
 if __name__=='__main__':
@@ -98,6 +115,8 @@ if __name__=='__main__':
     parser.add_argument('--get_classified_list', action='store_true')
     parser.add_argument('--pairwise_attention_modalities', action='store_true')
     parser.add_argument('--vanilla_fusion', action='store_true')    
+    parser.add_argument('--modalities',nargs='+',default='video audio text',help='Add modality names out of video, audio, text')
+
     
     args = parser.parse_args()
     root_dir_path = args.root_dir_path
@@ -105,6 +124,7 @@ if __name__=='__main__':
     get_classified_list = args.get_classified_list
     pairwise_attention_modalities = args.pairwise_attention_modalities
     vanilla_fusion = args.vanilla_fusion
+    modalities = args.modalities.split(' ') if args.modalities=='video audio text' else args.modalities
 
     non_encoded_videos_path = os.path.join(root_dir_path,'processed_data/non_encoded_videos')
     val_encoded_videos_pkl = 'runs/{}/val_encoded_video.pkl'.format(experiment_name)
@@ -113,6 +133,6 @@ if __name__=='__main__':
     EncodeVideo_obj = EncodeVideo()
 
     if get_classified_list:
-        inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, checkpoint_path, root_dir_path, EncodeVideo_obj, vanilla_fusion, pairwise_attention_modalities, get_classified_list=get_classified_list, experiment_name=experiment_name)
+        inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, checkpoint_path, root_dir_path, EncodeVideo_obj, modalities, vanilla_fusion, pairwise_attention_modalities, get_classified_list=get_classified_list, experiment_name=experiment_name)
     else:
-        inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, checkpoint_path, root_dir_path, EncodeVideo_obj, vanilla_fusion, pairwise_attention_modalities, experiment_name=experiment_name)
+        inference_on_val(non_encoded_videos_path, val_encoded_videos_pkl, classes, checkpoint_path, root_dir_path, EncodeVideo_obj, modalities, vanilla_fusion, pairwise_attention_modalities, experiment_name=experiment_name)
